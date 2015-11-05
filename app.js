5841bcff0006c7ce5db5605f67acd5d502098ea1
Shawn Petros <pet7915@rd230.emeraldhost.net>
2015-11-04 21:16:08 -0600

    it didn't like ES6 syntax :(

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-let express = require('express');
-let path = require('path');
-let app = express();
+var express = require('express');
+var path = require('path');
+var app = express();
 
 app.get('/', function (req, res) {
   res.status(200).send('Hello Surf!');
@@ -8,9 +8,9 @@ app.get('/', function (req, res) {
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-let server = app.listen(3000, function () {
-    let host = server.address().address;
-    let port = server.address().port;
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
 
     console.log('Surf app listening at http://%s:%s', host, port);
 });
