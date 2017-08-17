182a6d4819da12f94c01699110638ef74d730355
Shawn Petros <arashcuzi@gmail.com>
2017-08-17 13:38:21 -0500

    refactor

--- config/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    database: {
-        host: 'ds037095.mongolab.com:37095',
-        port: 27010,
-        db: 'surf',
-        username: 'admin',
-        password: 'admin'
-    }
-};
